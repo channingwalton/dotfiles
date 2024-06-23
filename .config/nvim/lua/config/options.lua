cf9c9b9ffa52869c7af8be0b327b0985b97e653d
Channing Walton <channingwalton@mac.com>
2024-06-23 22:11:31 +0100

    nvim mac leaders explicit for dotfyle

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- Disable virtual_text since it's redundant due to lsp_lines.
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 vim.opt.cursorline = false
 vim.opt.confirm = false
 vim.opt.spell = false
