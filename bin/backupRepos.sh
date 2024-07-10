a13d194507b5d52b5c99a022f43010bd764379ae
Channing Walton <channingwalton@mac.com>
2024-07-10 09:18:39 +0100

    updates

--- bin/backupRepos.sh
+++ bin/backupRepos.sh
@@ -12,7 +12,7 @@ export ARCHIVE=$DOCUMENTS/Archive/Code/git
 
 # GIT mirrors created with git clone --mirror https://github.com/lancewalton/treelog.git
 # to see what dir is being processed, put the following before the -exec: -exec echo {} \;
-find ${ARCHIVE} -name "*.git" -type d -exec /Users/channing/.nix-profile/bin/git -C {} remote update \;
+find ${ARCHIVE} -name "*.git" -type d -exec $HOME_DIR/.nix-profile/bin/git -C {} remote update \;
 
 # finished
 echo "Repo backup Complete at $(date)"
