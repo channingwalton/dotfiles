50c79acf1811bdb87b4852c972c7e876cb0dd0a9
Channing Walton <channing@Channings-2021-MacBook-Pro.local>
2024-08-16 23:27:14 +0100

    removing more nix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.g.python3_host_prog = "~/.nix-profile/bin/python3"
+vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
