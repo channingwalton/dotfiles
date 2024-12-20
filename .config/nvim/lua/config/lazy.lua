214401231aabaca4993e3c600cee39040c066a16
Channing Walton <channingwalton@mac.com>
2024-12-20 21:12:15 +0000

    treesitter-context

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -39,6 +39,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.scala" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     -- import/override with your plugins
     { import = "plugins" },
   },
