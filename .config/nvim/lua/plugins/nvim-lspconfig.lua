08a825e2b34ee25a848822da7289af50073a49b5
Channing Walton <channingwalton@mac.com>
2024-12-26 22:53:21 +0000

    Set metals capabilities for blink

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -67,6 +67,25 @@ return {
               "-Xmx2G",
             },
           },
+          setup = {
+            metals = function(_, opts)
+              local metals = require("metals")
+              local metals_config = vim.tbl_deep_extend("force", metals.bare_config(), opts)
+              metals_config.on_attach = LazyVim.has("nvim-dap") and metals.setup_dap or nil
+
+              local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+              vim.api.nvim_create_autocmd("FileType", {
+                pattern = { "scala", "sbt" },
+                callback = function()
+                  metals.initialize_or_attach(metals_config)
+                end,
+                group = nvim_metals_group,
+              })
+              -- From https://discord.com/channels/632642981228314653/632841743435563028/1319026390196158576
+              metals_config.capabilities = require("blink.cmp").get_lsp_capabilities()
+              return true
+            end,
+          },
         },
       },
     },
