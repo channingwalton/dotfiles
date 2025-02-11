47f3ca021a0d4e7d4ec86ad8b3c27b99616b0838
Channing Walton <channingwalton@mac.com>
2025-02-11 21:30:20 +0000

    Configure snacks

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,1 +1,17 @@
-return { "folke/snacks.nvim", opts = { words = { enabled = true } } }
+return {
+  "folke/snacks.nvim",
+  opts = {
+    words = {
+      enabled = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          exclude = { "node_modules", ".git" },
+        },
+      },
+    },
+  },
+}
