bccbfa35e2ebc9abf9b8c335bdbc40f17110303c
Channing Walton <channingwalton@mac.com>
2025-05-30 23:34:43 +0100

    configure zed task with bloop

---
+++ .config/zed/run-bloop-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+test_path=$1
+filename=$2
+
+# Check if path starts with foo/src pattern, if so use foo as project_name, otherwise use root
+if [[ $test_path == */src/* ]]; then
+  project_name=$(echo "$test_path" | cut -d'/' -f1)
+else
+  project_name="root"
+fi
+
+bloop test $project_name -o "*$filename*"
