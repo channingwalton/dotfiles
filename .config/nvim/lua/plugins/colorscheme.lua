c8639a0b8c59ba699eb2f1e8e68f5833c8582986
Channing Walton <channingwalton@mac.com>
2025-06-25 22:58:58 +0100

    tokyo night config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -9,7 +9,12 @@ return {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = {
+      transparent = true,
       style = "night",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
 }
