76138a3dd16fbd160c4749d39501294fd32d878a
Channing Walton <channingwalton@mac.com>
2025-09-15 17:29:39 +0100

    fzf-lua to show hidden files in rg

---
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,9 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = {
+    grep = {
+      rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e",
+      hidden = true,
+    },
+  },
+}
