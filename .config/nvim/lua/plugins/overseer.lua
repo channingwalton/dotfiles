75a2cd7a17ff46d0b951fa87b7660a2e53d2b159
Channing Walton <channingwalton@mac.com>
2025-09-20 10:27:06 +0100

    Claude fixed my config

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -1,63 +1,63 @@
 return {
   "stevearc/overseer.nvim",
-  require("overseer").register_template({
-    name = "sbt test",
-    params = {},
-    condition = {
-      -- This makes the template only available in the current directory
-      -- In case you :cd out later
-      dir = vim.fn.getcwd(),
-    },
-    builder = function()
-      return {
-        cmd = { "sbt" },
-        args = { "test" },
-      }
-    end,
-  }),
-  require("overseer").register_template({
-    name = "sbt integration tests",
-    params = {},
-    condition = {
-      -- This makes the template only available in the current directory
-      -- In case you :cd out later
-      dir = "~/dev/sxm",
-    },
-    builder = function()
-      return {
-        cmd = { "sbt" },
-        args = { "it" },
-      }
-    end,
-  }),
-  require("overseer").register_template({
-    name = "sbt format",
-    params = {},
-    condition = {
-      -- This makes the template only available in the current directory
-      -- In case you :cd out later
-      dir = vim.fn.getcwd(),
-    },
-    builder = function()
-      return {
-        cmd = { "sbt" },
-        args = { "scalafmtFormatAll" },
-      }
-    end,
-  }),
-  require("overseer").register_template({
-    name = "sbt updates",
-    params = {},
-    condition = {
-      -- This makes the template only available in the current directory
-      -- In case you :cd out later
-      dir = vim.fn.getcwd(),
-    },
-    builder = function()
-      return {
-        cmd = { "sbt" },
-        args = { "dependencyUpdates; reload plugins; dependencyUpdates" },
-      }
-    end,
-  }),
+  config = function()
+    local overseer = require("overseer")
+    
+    -- Register custom templates
+    overseer.register_template({
+      name = "sbt test",
+      params = {},
+      condition = {
+        dir = vim.fn.getcwd(),
+      },
+      builder = function()
+        return {
+          cmd = { "sbt" },
+          args = { "test" },
+        }
+      end,
+    })
+
+    overseer.register_template({
+      name = "sbt integration tests",
+      params = {},
+      condition = {
+        dir = "~/dev/sxm",
+      },
+      builder = function()
+        return {
+          cmd = { "sbt" },
+          args = { "it" },
+        }
+      end,
+    })
+
+    overseer.register_template({
+      name = "sbt format",
+      params = {},
+      condition = {
+        dir = vim.fn.getcwd(),
+      },
+      builder = function()
+        return {
+          cmd = { "sbt" },
+          args = { "scalafmtFormatAll" },
+        }
+      end,
+    })
+
+    overseer.register_template({
+      name = "sbt updates",
+      params = {},
+      condition = {
+        dir = vim.fn.getcwd(),
+      },
+      builder = function()
+        return {
+          cmd = { "sbt" },
+          args = { "dependencyUpdates; reload plugins; dependencyUpdates" },
+        }
+      end,
+    })
+  end,
 }
